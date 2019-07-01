ba2f5d555ecd4c66f9faa5beb479f3844afdfe81
Awtian <me@awtian.com>
2019-07-01 15:47:55 +0700

    fixing cunsumer

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ env.config()
 
 var T = new Twit({
   consumer_key:         process.env.CONSUMER_KEY,
-  consumer_secret:      process.env.CUNSUMER_SECRET,
+  consumer_secret:      process.env.CONSUMER_SECRET,
   access_token:         process.env.ACCESS_TOKEN,
   access_token_secret:  process.env.ACCESS_TOKEN_SECRET
 })
