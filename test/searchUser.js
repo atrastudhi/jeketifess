62dc3417aa63fc148e3324a4fa250bb69b1486c9
Atras Tudhi <muh.saidi@bizzy.co.id>
2019-07-29 15:50:23 +0700

    test

--- test/searchUser.js
+++ test/searchUser.js
@@ -1,15 +1,19 @@
-var Twit = require('twit');
-const env = require('dotenv');
+// var Twit = require('twit');
+// const env = require('dotenv');
 
-env.config()
+// env.config()
 
-var T = new Twit({
-  consumer_key:         process.env.CONSUMER_KEY,
-  consumer_secret:      process.env.CONSUMER_SECRET,
-  access_token:         process.env.ACCESS_TOKEN,
-  access_token_secret:  process.env.ACCESS_TOKEN_SECRET
-})
+// var T = new Twit({
+//   consumer_key:         process.env.CONSUMER_KEY,
+//   consumer_secret:      process.env.CONSUMER_SECRET,
+//   access_token:         process.env.ACCESS_TOKEN,
+//   access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+// })
 
-T.get('users/lookup', { user_id: '551679581' }, (err, data, response) => {
-    console.log(data)
-})
+// T.get('users/lookup', { user_id: '551679581' }, (err, data, response) => {
+//     console.log(data)
+// })
+
+for (var i = 0; i < 5; i++) {
+  console.log(i)
+}
